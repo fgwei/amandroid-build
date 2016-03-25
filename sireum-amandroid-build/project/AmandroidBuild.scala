3620aa0e522fc0bf9e239adcfa390dc1188c0414
Fengguo Wei <wfg611004900521@gmail.com>
2016-03-25 17:59:53 -0400

    fix classpath

--- sireum-amandroid-build/project/AmandroidBuild.scala
+++ sireum-amandroid-build/project/AmandroidBuild.scala
@@ -7,13 +7,6 @@
  * 
  * Detailed contributors are listed in the CONTRIBUTOR.md
  ******************************************************************************/
-/*
-Copyright (c) 2013-2014 Fengguo Wei & Sankardas Roy, Kansas State University.        
-All rights reserved. This program and the accompanying materials      
-are made available under the terms of the Eclipse Public License v1.0 
-which accompanies this distribution, and is available at              
-http://www.eclipse.org/legal/epl-v10.html                             
-*/
 import sbt._
 import Keys._
 import sbt.complete.Parsers._
